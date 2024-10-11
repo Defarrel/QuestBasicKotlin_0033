3a794d960d831a1160c2b6f57e2d3664a81f02ab
defarrel <Defarrel>
2024-10-11 17:24:26 +0700

    first commit

---
+++ app/src/main/java/com/example/pam_tugas1/TipeData.kt
@@ -0,0 +1,43 @@
+package com.example.pam_tugas1
+
+fun main(){
+
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    //Double
+    val doableValue: Double = 60.6
+    println("Double: $doableValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    //boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //string
+    var stringValue: String = "Hello World"
+    println("String: $stringValue")
+
+    stringValue = "Hello sayang"
+    println("String: $stringValue")
+}
