6d8bb2a174a38c7bd9c00ef0b91873daf1f83e48
defarrel <Defarrel>
2024-10-11 17:53:07 +0700

    Menambahkan 1 fungsi lagi yaitu dengan return dan memanggilanya di fun main. ada kode tambahan yang saya tambahkan karena yang di ss tidak lengkap.

--- app/src/main/java/com/example/pam_tugas1/Functions.kt
+++ app/src/main/java/com/example/pam_tugas1/Functions.kt
@@ -27,3 +27,18 @@ fun withDefaultParameter(name: String = " Lilin", age: Int){
     println("Hello, $name Your are $age years old.")
 }
 
+// functions withreturn tidak di panggil di main
+fun withReturn(panjang: Int, lebar : Int){
+    println()
+    println("==withReturn==")
+    println("$panjang x $lebar =")
+    return println( panjang * lebar)
+}
+
+fun main(){
+    withoutParameter()
+    withParameter("Lilin")
+    withNamedArgument(name = "Lilin", age = 25)
+    withDefaultParameter(age = 25)
+    withReturn(10,20)
+}
